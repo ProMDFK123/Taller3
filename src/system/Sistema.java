641128194aa3f7ca600f10ff014b8f375362697a
Gabriel Lopez <gabriel.lopez@alumnos.ucn.cl>
2023-04-30 14:40:30 -0400

    system v2.0.0

--- src/system/Sistema.java
+++ src/system/Sistema.java
@@ -1,14 +1,13 @@
 package system;
 
-import com.opencsv.exceptions.CsvValidationException;
 import objects.Instrumento;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public interface Sistema {
-    void agregarInstrumento(String direccionArchivo) throws IOException, CsvValidationException;
-    void venderInstrumento(Instrumento instrumento);
+    void agregarInstrumento(String direccionArchivo) throws IOException;
+    void venderInstrumentoImpl(String codigo);
     String consultarInventario();
-    void cierre(boolean estado);
+    boolean cierre(boolean estado);
 }
--- src/system/SistemaImpl.java
+++ src/system/SistemaImpl.java
@@ -1,6 +1,5 @@
 package system;
 
-import com.opencsv.exceptions.CsvValidationException;
 import contenedores.ListaInstrumento;
 import edu.princeton.cs.stdlib.StdIn;
 import objects.Cuerda;
@@ -28,7 +27,7 @@ public class SistemaImpl implements Sistema{
     /**
      * Constructor del sistema.
      * @throws IOException
-     * @throws CsvValidationException
+
      */
     public SistemaImpl() throws IOException {
         totalInstrumentos = new ListaInstrumento(100);
@@ -41,7 +40,6 @@ public class SistemaImpl implements Sistema{
     /**
      * Menu principal del programa.
      * @throws IOException
-     * @throws CsvValidationException
      */
     public void menu() throws IOException {
         //variable booleana que comprueba si el programa esta encendido o no.
@@ -59,7 +57,7 @@ public class SistemaImpl implements Sistema{
             //registra la opcion escogida por el usuario.
             String opcion = StdIn.readString();
             switch (opcion) {
-                //ToDo: metodos correspondientes a cada caso
+                case "1" -> venderInstrumento();
             }
         }
     }
@@ -69,48 +67,76 @@ public class SistemaImpl implements Sistema{
      * ubicados en un archivo de tipo 'valores separados por coma' (CVS).
      * @param archivo a abrir.
      * @throws IOException
-     * @throws CsvValidationException
      */
     @Override
     public void agregarInstrumento(String archivo) throws IOException{
-        //
+        //lee el archivo cvs
         BufferedReader lectura = new BufferedReader(new FileReader(archivo));
+        //lee la primera linea del archivo
         String linea = lectura.readLine();
 
         while(linea!=null){
+            //Separa cada linea
             String[] atributos = linea.split(",");
 
+            /*
+            comprueba el tipo de instrumento,
+            cambia a integer las variables pertinentes para
+            crear un objeto instrumento, y lo almacena en sus
+            correspondientes contenedores.
+             */
             if(atributos[0]=="Viento"){
                 int precio = Integer.parseInt(atributos[2]);
                 int stock = Integer.parseInt(atributos[3]);
                 Instrumento viento = new Viento(atributos[1],precio,stock,atributos[4],atributos[5]);
                 this.totalInstrumentos.agregar(viento);
-                this.instrumentosViento.agregar(viento);
+                Viento v = (Viento) viento;
+                this.instrumentosViento.agregar(v);
             } else if (atributos[0]=="Cuerda") {
                 int precio = Integer.parseInt(atributos[2]);
                 int stock = Integer.parseInt(atributos[3]);
                 int cantCuerdas = Integer.parseInt(atributos[7]);
                 Instrumento cuerda = new Cuerda(atributos[1],precio,stock,atributos[4],atributos[5], atributos[6], cantCuerdas, atributos[8]);
                 this.totalInstrumentos.agregar(cuerda);
-                this.instrumentosCuerda.agregar(cuerda);
+                Cuerda c = (Cuerda) cuerda;
+                this.instrumentosCuerda.agregar(c);
             } else if (atributos[0]=="Percusion") {
                 int precio = Integer.parseInt(atributos[2]);
                 int stock = Integer.parseInt(atributos[3]);
                 Instrumento percusion = new Percusion(atributos[1],precio,stock,atributos[4],atributos[5], atributos[6],atributos[7]);
                 this.totalInstrumentos.agregar(percusion);
-                this.instrumentosPercusion.agregar(percusion);
+                Percusion p = (Percusion) percusion;
+                this.instrumentosPercusion.agregar(p);
             }
+            //lee otra linea.
             linea = lectura.readLine();
-        }
-
-        if(lectura!=null){
-            lectura.close();
         }
     }
 
+    /**
+     * Método que implementa la venta de un instrumento.
+     * @param codigo del instrumento a vender
+     */
     @Override
-    public void venderInstrumento(Instrumento instrumento) {
+    public void venderInstrumentoImpl(String codigo) {
+        //Revisa si el instrumento existe o no.
+        if(!this.totalInstrumentos.existe(codigo)){
+            System.out.println("El codigo ingresado no corresponde a ningun instrumento registrado.");
+            return;
+        }
+        //El instrumento existe y es guardado en una variable auxilar.
+        Instrumento instrumentoVendido = this.totalInstrumentos.buscar(codigo);
 
+        //Valida si aun hay stock de dicho instrumento o no.
+        if(instrumentoVendido.getStock()==0){
+            System.out.println("No queda stock de este instrumento,");
+            return;
+        }
+
+        //generación de una boleta.
+        this.generarBoleta(instrumentoVendido);
+        //Disminuye en 1 el stock del instrumento vendido.
+        instrumentoVendido.setStock(instrumentoVendido.getStock()-1);
     }
 
     @Override
@@ -118,11 +144,17 @@ public class SistemaImpl implements Sistema{
         return null;
     }
 
+    /**
+     * Método para cerrar el programa.
+     * @param estado del prograna
+     * @return false para que el programa finalice
+     */
     @Override
-    public void cierre(boolean estado) {
+    public boolean cierre(boolean estado) {return estado=false;}
 
-    }
-
+    /**
+     * Método auxiliar que despliega el menu principal del programa
+     */
     private void mainmenu(){
         StringBuilder sb = new StringBuilder();
         sb.append("* BIENVENID@ A BEAT THE RYTHM *");
@@ -136,5 +168,61 @@ public class SistemaImpl implements Sistema{
         sb.append("3. Salir.");
         String mainMenu = sb.toString();
         System.out.println(mainMenu);
+    }
+
+    /**
+     * Método auxiliar que genera la boleta de una venta.
+     * @param instrumento
+     */
+    public void generarBoleta(Instrumento instrumento){
+        //Listas que almacenan la información de los vendedores.
+        String[] CI = {"2825349", "2749256", "5465893","2578323","XX"};
+        String[] vendedores = {"Yolanda Castro","Matias Rios","Yassine Valenzuela","Brenda Arnau","SIN NOMBRE"};
+
+        //posición auxiliar.
+        int posAux = (int) (Math.random()*5);
+        //Recorre la lista hasta encontrar al vendedor, si este se ha registrado.
+        for(int i = 0;i<5;i++){
+            //Comprueba la info del vendedor.
+            if(i==posAux){
+                String ci = CI[i];
+                String vendedor = vendedores[i];
+
+                //generación de llaboleta.
+                StringBuilder boleta = new StringBuilder();
+                boleta.append("BEAT THE RYTHM\n");
+                boleta.append("----------------------------------------------------------------\n");
+                boleta.append("RUC O CI: "+ci+"\n");
+                boleta.append("----------------------------------------------------------------\n");
+                boleta.append("NOMBRE VENDEDOR: "+vendedor+"\n");
+                boleta.append("----------------------------------------------------------------\n");
+                boleta.append("CANTIDAD | DESCRIPCIÓN | PRECIO UNITARIO | IVA% | VALOR DE VENTA\n");
+                boleta.append("----------------------------------------------------------------\n");
+                boleta.append("1 | "+instrumento.getNombre()+" | "+instrumento.getPrecio()+" | 19% | "+instrumento.getPrecio());
+                boleta.append("----------------------------------------------------------------\n");
+                boleta.append("TOTAL A PAGAR: "+instrumento.getPrecio());
+            }
+        }
+    }
+
+    /**
+     * Método del menú principal de una venta.
+     */
+    public void venderInstrumento(){
+        //codigo a ingresar.
+        String code;
+
+        //menú principal.
+        StringBuilder vender = new StringBuilder();
+        vender.append("=========================\n");
+        vender.append("= VENDER UN INSTRUMENTO =\n");
+        vender.append("=========================\n");
+        vender.append("\n");
+        vender.append("Ingrese el código del instrumento: ");
+
+        //Registro del código.
+        code = StdIn.readString();
+        //Realización de la venta.
+        this.venderInstrumentoImpl(code);
     }
 }
